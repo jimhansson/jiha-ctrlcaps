b9fb229c3d5d05c83afaa83a2bba560c1beb5118
Jim Hansson <JIHA@powel.com>
2016-08-08 12:03:44 +0200

    updated documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,31 +1,14 @@
 # == Class: ctrlcaps
 #
-# Full description of class ctrlcaps here.
+# sets the scancode map in the registry to change the left ctrl for capslock.
 #
 # === Parameters
 #
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
 # === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
 #
 # === Examples
 #
-#  class { 'ctrlcaps':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+#  class { 'ctrlcaps': }
 #
 # === Authors
 #
